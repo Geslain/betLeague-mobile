51a5a9cdba23652f2b0de1938b185dc2e452cc47
Geslain Dahan <geslain.dahan@keyrus.com>
2018-04-19 17:12:59 +0200

    add multi match event bet

--- src/pages/prognosis-event/prognosis-event.ts
+++ src/pages/prognosis-event/prognosis-event.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {HomePage} from "../home/home";
 import {TeamsPage} from "../teams/teams";
+import {HttpClient} from "@angular/common/http";
 
 /**
  * Generated class for the PrognosisEventPage page.
@@ -16,15 +17,32 @@ import {TeamsPage} from "../teams/teams";
 })
 export class PrognosisEventPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private matches;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PrognosisEventPage');
   }
 
+  ngOnInit() {
+    this.fetchMatches()
+  }
+
   back() {
     this.navCtrl.push(TeamsPage)
   }
 
+  submit() {
+
+  }
+
+  fetchMatches() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    this.http.get("https://betleague-api.herokuapp.com/event/5ad86196fef554014cd2d4d0/getAllMatches").subscribe((matches) => {
+      this.matches = matches;
+    });
+  }
 }
